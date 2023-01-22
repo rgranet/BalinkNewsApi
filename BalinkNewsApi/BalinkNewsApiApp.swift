db9480ed5721ad31ff146dcab90b321e0a133c20
Ruben Granet <ruben.granet@icloud.com>
2023-01-22 21:50:53 +0200

    Initial Commit

---
+++ BalinkNewsApi/BalinkNewsApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalinkNewsApiApp.swift
+//  BalinkNewsApi
+//
+//  Created by Ruben Granet on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BalinkNewsApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
